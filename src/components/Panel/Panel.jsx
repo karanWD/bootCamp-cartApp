81152f906e6629412b0b811e3e7dd070b4967dd8
karan_shams_pirzadeh <karanshamspirzadeh@gmail.com>
2022-04-12 11:47:41 +0430

    implement cart app with localStorage

--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -1,12 +1,15 @@
 import React, {useState} from "react";
 import {Slider, Form, Input, InputNumber, Col, Button, Space} from "antd";
 
-const Panel = ({setFormData}) => {
-
-    const [price,setPrice] = useState(0)
-    const [disc,setDisc] = useState(0)
-    const finishHandler = (values) =>{
+const Panel = ({data,setFormData}) => {
+    const [price, setPrice] = useState(0)
+    const [disc, setDisc] = useState(0)
+    const finishHandler = (values) => {
         setFormData((prev)=>[...prev,values])
+        data ?
+            localStorage.setItem("cart",JSON.stringify([...data, values]))
+            :
+            localStorage.setItem("cart", JSON.stringify([values]))
     }
     return (
         <>
@@ -17,21 +20,24 @@ const Panel = ({setFormData}) => {
                     </Form.Item>
                 </Form.Item>
                 <Form.Item label="price" labelAlign={"left"} labelCol={{span: 24}}>
-                    <Form.Item name="price" rules={[{required: true, message: "Price is Required"}]} >
-                        <InputNumber onChange={value => setPrice(value)} size={"large"} style={{width: "100%"}} controls={false} min={0} placeholder="enter your product name"/>
+                    <Form.Item name="price" rules={[{required: true, message: "Price is Required"}]}>
+                        <InputNumber onChange={value => setPrice(value)} size={"large"} style={{width: "100%"}}
+                                     controls={false} min={0} placeholder="enter your product name"/>
                     </Form.Item>
                 </Form.Item>
                 <Form.Item label="discount" name={"discount"} labelAlign={"left"} labelCol={{span: 24}}>
-                    <Slider tipFormatter={(value)=>`${value}%`} onChange={value => setDisc(value)} min={0} max={100} defaultValue={0}/>
+                    <Slider tipFormatter={(value) => `${value}%`} onChange={value => setDisc(value)} min={0} max={100}
+                            defaultValue={0}/>
                 </Form.Item>
                 <Form.Item>
                     <span>Final Price :</span>
                     <span style={{padding: "0 5px", fontWeight: "bold", fontSize: "24px"}}>
-                        {price - (price*disc/100)} $
+                        {price - (price * disc / 100)} $
                     </span>
                 </Form.Item>
                 <Form.Item>
-                    <Button htmlType={"submit"} shape={"round"} type="primary" block style={{marginTop: "30px", height: "50px"}} size={"large"}>
+                    <Button htmlType={"submit"} shape={"round"} type="primary" block
+                            style={{marginTop: "30px", height: "50px"}} size={"large"}>
                         Add
                     </Button>
                 </Form.Item>
